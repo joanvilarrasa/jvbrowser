ae1c675caf3784acab096cf880b80755b9dafc47
joanvilarrasa <joan.vilarrasa96@gmail.com>
2025-07-17 22:11:00 +0200

    Chapter_2-Tutorial-1-2 Rendering and scrollign done

--- browser.py
+++ browser.py
@@ -1,261 +1,64 @@
-import socket
-import ssl
-import time 
-import gzip
+from url import URL, lex
+import tkinter
 
-active_sockets = {}
-responses_cache = {}
-MAX_REDIRECTS = 10
+WIDTH, HEIGHT = 800, 600
+HSTEP, VSTEP = 13, 18
+SCROLL_STEP = 100
 
-class URL:    
-    def __init__(self, url):
-        self.view_source = False
-        self.scheme = None
-        self.mediatype = None
-        self.data = None
-        self.host = None
-        self.path = None
-        self.port = None
-        self.method = "GET"
+class Browser:
+    def __init__(self):
+        self.window = tkinter.Tk()
+        self.canvas = tkinter.Canvas(
+            self.window, 
+            width=WIDTH,
+            height=HEIGHT
+        )
+        self.canvas.pack()
+        self.scroll = 0
+        self.display_list = []
+        self.window.bind("<Down>", self.scrolldown)
+        self.window.bind("<Up>", self.scrollup)
+        self.window.bind("<Button-4>", self.scrollup)
+        self.window.bind("<Button-5>", self.scrolldown)
 
-        # If the URL starts with view-source: set the flag and remove the scheme
-        if url.startswith("data:"):
-            self.scheme = "data"
-            self.mediatype, self.data = url.split(",", 1)
-            return
+    def load(self, url):
+        body = url.request()
+        text = lex(body)
+        self.display_list = layout(text)
+        self.draw()
 
-        if url.startswith("view-source:"):
-            # If the URL starts with view-source: set the flag and remove the scheme
-            self.view_source = True
-            url = url[12:]
-        else:
-            self.view_source = False
+    def draw(self):
+        self.canvas.delete("all")
+        for x, y, c in self.display_list:
+            if y > self.scroll + HEIGHT: continue
+            if y + VSTEP < self.scroll: continue
+            self.canvas.create_text(x, y - self.scroll, text=c)
 
-        # Split the URL into scheme, host, and path
-        self.scheme, url = url.split("://", 1)
-        assert self.scheme in ["http", "https", "file", "data"]
-        # Set the default port for the scheme
-        if self.scheme == "http":
-            self.port = 80
-        elif self.scheme == "https":
-            self.port = 443
-        elif self.scheme == "file":
-            self.port = None
-        elif self.scheme == "data":
-            self.port = None
+    def scrolldown(self, e):
+        self.scroll += SCROLL_STEP
+        self.draw()
 
-        # Parse the hoset and the path
-        if "/" not in url:
-            url = url + "/"
-        self.host, url = url.split("/", 1)
-        self.path = "/" + url
-        # If the host contains a port, use it instead of the default port
-        if ":" in self.host:
-            self.host, port = self.host.split(":", 1)
-            self.port = int(port)
+    def scrollup(self, e):
+        if self.scroll > 0:
+            self.scroll -= SCROLL_STEP
+            self.draw()
 
-    def request(self):
-        if self.scheme == "data":
-            return self.return_request(self.data)
-        if self.host is None:
-            raise ValueError("Invalid host: {}".format(self.host))
+def layout(text):
+    display_list = []
+    cursor_x, cursor_y = HSTEP, VSTEP
+    for c in text:
+        display_list.append((cursor_x, cursor_y, c))
+        cursor_x += HSTEP
+        if c == "\n":
+            cursor_y += VSTEP * 1.5
+            cursor_x = HSTEP
+        elif cursor_x >= WIDTH - HSTEP:
+            cursor_y += VSTEP
+            cursor_x = HSTEP
+    return display_list
 
-        # If the scheme is file, open the file specified by combining the host and path as a binary
-        if self.scheme == "file":
-            with open(self.host + self.path, encoding="utf8", newline="\r\n") as f:
-                response = f.read()
-                return self.return_request(response)
-
-        # Send the request to the server
-        request = self.build_request()
-
-        cached_response = None
-        if self.method == "GET":
-            cached_response = responses_cache.get(self.host + self.path)
-            if cached_response is not None:
-                if cached_response["expires"] > time.time():
-                    # Cache hit
-                    return self.return_request(content = cached_response["content"])
-
-        # Moved the socket after the cache check to avoid opening a connection if the request is cached
-        s = self.get_open_socket()
-        s.send(request.encode("utf8"))
-        
-        # Read the response from the server
-        response = s.makefile("rb")
-        version, status, explanation, response_headers = self.get_response_metadata(response)
-        # Handle redirects
-        if int(status) >= 300 and int(status) < 400:
-            return self.return_request(redirect_url = self.get_response_redirect_url(response_headers))
-
-        # Get the content of the response
-        content = self.get_response_content(response, response_headers)
-
-        # If possible, cache the response
-        if int(status) == 200 and self.method == "GET":
-            self.cache_response(content, response_headers)
-
-        # Return the content of the response
-        return self.return_request(content)
-
-    def get_open_socket(self): 
-        connection_key = (self.host, self.port, self.scheme)
-        # See if the connection exists
-        s = active_sockets.get(connection_key)
-        if s is None:
-            s = socket.socket(
-                family=socket.AF_INET,
-                type=socket.SOCK_STREAM,
-                proto=socket.IPPROTO_TCP,
-            )
-            s.connect((self.host, self.port))
-            # Wrap the socket in an SSL context if the scheme is https
-            if self.scheme == "https":
-                ctx = ssl.create_default_context()
-                s = ctx.wrap_socket(s, server_hostname=self.host)
-            # Store the connection
-            active_sockets[connection_key] = s
-        return s
-    
-    def build_request(self):
-        request = "{} {} HTTP/1.1\r\n".format(self.method, self.path)
-        request += "Host: {}\r\n".format(self.host)
-        request += "Connection: keep-alive\r\n"
-        request += "User-Agent: jvbrowser/1.0\r\n"
-        request += "Accept-Encoding: gzip\r\n"
-        request += "\r\n"
-
-        return request
-
-    def get_response_metadata(self, response):
-        statusline = response.readline().decode("utf8")
-        version, status, explanation = statusline.split(" ", 2)
-        response_headers = {}
-        while True:
-            line = response.readline().decode("utf8")
-            if line == "\r\n": break
-            header, value = line.split(":", 1)
-            response_headers[header.casefold()] = value.strip()
-        assert "transfer-encoding" not in response_headers
-        return version, status, explanation, response_headers
-
-    def get_response_content(self, response, response_headers):
-        if "content-encoding" in response_headers:
-            assert response_headers["content-encoding"] == "gzip"
-            if "content-length" in response_headers:
-                content_length = int(response_headers["content-length"])
-                compressed_content = response.read(content_length)
-            else:
-                # If no content length, read until connection closes
-                compressed_content = response.read()
-            content = gzip.decompress(compressed_content).decode("utf8")
-        elif "content-length" in response_headers:
-            content_length = int(response_headers["content-length"])
-            content = response.read(content_length).decode("utf8")
-        else:
-            # If we did not find content length, read all the content
-            print("No content length")
-            content = response.read().decode("utf8")
-        return content
-
-    def get_response_redirect_url(self, response_headers):
-        assert "location" in response_headers
-        assert self.scheme is not None
-        assert self.host is not None
-        redirect_url = response_headers["location"]
-        # If the redirect URL is relative, make it absolute
-        if redirect_url.startswith("/") and self.scheme is not None:
-            redirect_url = self.scheme + "://" + self.host + redirect_url
-        return redirect_url
-    
-    def cache_response(self, content, response_headers):
-        if self.host is not None and responses_cache.get(self.host + self.path) is None:
-            assert "cache-control" in response_headers
-            cache_control = response_headers["cache-control"]
-            # If the cache control contains max-age or no-cache, 
-            if "max-age" in cache_control:
-                print("Caching response for {} seconds".format(cache_control.split("=")[1]))
-                responses_cache[self.host + self.path] = {
-                    "content": content,
-                    "response_headers": response_headers,
-                    "created": time.time(),
-                    "expires": time.time() + int(cache_control.split("=")[1])
-                }
-            # Otherwise do not cache the response
-    
-    def return_request(self, content = None, view_source = None, redirect_url = None):
-        return content, view_source, redirect_url
-
-def show(body, view_source):
-    if not body.startswith("<"):
-        print(body)
-        return
-
-    # If the view source flag is set, do not parse the body as HTML. Just print it as is.
-    if view_source:
-        print(body)
-        return
-
-    # TODO: This code is trash, maybe improve it at some point
-    in_tag = False
-    in_entity = False
-    entity = ""
-    for c in body:
-        if c == "<":
-            in_tag = True
-        elif c == ">":
-            in_tag = False
-        elif c == "&":
-            entity = "&"
-            in_entity = True
-        elif c == ";":
-            entity += c
-            decoded_entity = decode_entity(entity)
-            if decoded_entity is not None:
-                print(decoded_entity, end="")
-            else:
-                print(entity, end="")
-            entity = ""
-            in_entity = False
-        elif in_entity:
-            entity += c
-            if len(entity) > 5:
-                print(entity, end="")
-                entity = ""
-                in_entity = False      
-        elif not in_tag and not in_entity:
-            print(c, end="")
-
-def decode_entity(entity):
-    if entity == "&lt;":
-        return "<"
-    elif entity == "&gt;":
-        return ">"
-    else:
-        return None
-
-def load(url):
-    body, view_source, redirect_url = url.request()
-
-    redirects = 0
-    while redirect_url is not None:
-        body, view_source, redirect_url = URL(redirect_url).request()
-        redirects += 1
-        if redirects > MAX_REDIRECTS:
-            raise Exception("Too many redirects")
-    show(body, view_source)
-
-    # Test the cache
-    body2, view_source2, redirect_url2 = url.request()
-
-    redirects = 0
-    while redirect_url2 is not None:
-        body2, view_source2, redirect_url2 = URL(redirect_url2).request()
-        redirects += 1
-        if redirects > MAX_REDIRECTS:
-            raise Exception("Too many redirects")
-    show(body2, view_source2)
 
 if __name__ == "__main__":
     import sys
-    load(URL(sys.argv[1]))
+    Browser().load(URL(sys.argv[1]))
+    tkinter.mainloop()
---
+++ url.py
@@ -0,0 +1,274 @@
+import socket
+import ssl
+import time 
+import gzip
+
+active_sockets = {}
+responses_cache = {}
+MAX_REDIRECTS = 10
+
+class URL:    
+    def __init__(self, url):
+        self.view_source = False
+        self.scheme = None
+        self.mediatype = None
+        self.data = None
+        self.host = None
+        self.path = None
+        self.port = None
+        self.method = "GET"
+        self.redirects = 0
+        self.init_url(url)
+
+    def init_url(self, url):
+        # If the URL starts with view-source: set the flag and remove the scheme
+        if url.startswith("data:"):
+            self.scheme = "data"
+            self.mediatype, self.data = url.split(",", 1)
+            return
+
+        if url.startswith("view-source:"):
+            # If the URL starts with view-source: set the flag and remove the scheme
+            self.view_source = True
+            url = url[12:]
+        else:
+            self.view_source = False
+
+        # Split the URL into scheme, host, and path
+        self.scheme, url = url.split("://", 1)
+        assert self.scheme in ["http", "https", "file", "data"]
+        # Set the default port for the scheme
+        if self.scheme == "http":
+            self.port = 80
+        elif self.scheme == "https":
+            self.port = 443
+        elif self.scheme == "file":
+            self.port = None
+        elif self.scheme == "data":
+            self.port = None
+
+        # Parse the hoset and the path
+        if "/" not in url:
+            url = url + "/"
+        self.host, url = url.split("/", 1)
+        self.path = "/" + url
+        # If the host contains a port, use it instead of the default port
+        if ":" in self.host:
+            self.host, port = self.host.split(":", 1)
+            self.port = int(port)
+    
+    def request(self):
+        if self.scheme == "data":
+            return self.data
+        if self.host is None:
+            raise ValueError("Invalid host: {}".format(self.host))
+
+        # If the scheme is file, open the file specified by combining the host and path as a binary
+        if self.scheme == "file":
+            with open(self.host + self.path, encoding="utf8", newline="\r\n") as f:
+                response = f.read()
+                return response
+
+
+        # Send the request to the server
+        request = self.build_request()
+
+        cached_response = None
+        if self.method == "GET":
+            cached_response = responses_cache.get(self.host + self.path)
+            if cached_response is not None:
+                if cached_response["expires"] > time.time():
+                    # Cache hit
+                    return cached_response["content"]
+
+        # Moved the socket after the cache check to avoid opening a connection if the request is cached
+        s = self.get_open_socket()
+        s.send(request.encode("utf8"))
+        
+        # Read the response from the server
+        response = s.makefile("rb")
+        version, status, explanation, response_headers = self.get_response_metadata(response)
+        # Handle redirects
+        if int(status) >= 300 and int(status) < 400:
+            content = self.handle_redirect(response_headers)
+            self.redirects = 0
+            return content
+
+        # Get the content of the response
+        content = self.get_response_content(response, response_headers)
+
+        # If possible, cache the response
+        if int(status) == 200 and self.method == "GET":
+            self.cache_response(content, response_headers)
+
+        # Return the content of the response
+        return content
+
+    def get_open_socket(self): 
+        connection_key = (self.host, self.port, self.scheme)
+        # See if the connection exists
+        s = active_sockets.get(connection_key)
+        if s is None:
+            s = socket.socket(
+                family=socket.AF_INET,
+                type=socket.SOCK_STREAM,
+                proto=socket.IPPROTO_TCP,
+            )
+            s.connect((self.host, self.port))
+            # Wrap the socket in an SSL context if the scheme is https
+            if self.scheme == "https":
+                ctx = ssl.create_default_context()
+                s = ctx.wrap_socket(s, server_hostname=self.host)
+            # Store the connection
+            active_sockets[connection_key] = s
+        return s
+    
+    def build_request(self):
+        request = "{} {} HTTP/1.1\r\n".format(self.method, self.path)
+        request += "Host: {}\r\n".format(self.host)
+        request += "Connection: keep-alive\r\n"
+        request += "User-Agent: jvbrowser/1.0\r\n"
+        request += "Accept-Encoding: gzip\r\n"
+        request += "\r\n"
+
+        return request
+
+    def get_response_metadata(self, response):
+        statusline = response.readline().decode("utf8")
+        version, status, explanation = statusline.split(" ", 2)
+        response_headers = {}
+        while True:
+            line = response.readline().decode("utf8")
+            if line == "\r\n": break
+            header, value = line.split(":", 1)
+            response_headers[header.casefold()] = value.strip()
+        return version, status, explanation, response_headers
+
+    def get_response_content(self, response, response_headers):
+        # Handle transfer-encoding first
+        if "transfer-encoding" in response_headers:
+            transfer_encoding = response_headers["transfer-encoding"]
+            if transfer_encoding == "chunked":
+                raw_content = self.read_chunked_content(response)
+            else:
+                raise ValueError(f"Unsupported transfer encoding: {transfer_encoding}")
+        elif "content-length" in response_headers:
+            content_length = int(response_headers["content-length"])
+            raw_content = response.read(content_length)
+        else:
+            # If we did not find content length, read all the content
+            print("No content length")
+            raw_content = response.read()
+        
+        # Handle content-encoding after getting the raw content
+        if "content-encoding" in response_headers:
+            assert response_headers["content-encoding"] == "gzip"
+            content = gzip.decompress(raw_content).decode("utf8")
+        else:
+            content = raw_content.decode("utf8")
+        
+        return content
+
+    def read_chunked_content(self, response):
+        """Read content using chunked transfer encoding."""
+        chunks = []
+        while True:
+            # Read the chunk size line
+            chunk_size_line = response.readline().decode("utf8").strip()
+            if not chunk_size_line:
+                continue
+            
+            # Parse the chunk size (hexadecimal)
+            chunk_size = int(chunk_size_line.split(';')[0], 16)
+            
+            # If chunk size is 0, we've reached the end
+            if chunk_size == 0:
+                break
+            
+            # Read the chunk data
+            chunk_data = response.read(chunk_size)
+            chunks.append(chunk_data)
+            
+            # Read the CRLF after the chunk
+            response.readline()
+        
+        # Combine all chunks and return as bytes
+        content = b''.join(chunks)
+        return content
+
+    def handle_redirect(self, response_headers):
+        if self.redirects > MAX_REDIRECTS:
+            raise Exception("Too many redirects")
+        self.redirects += 1
+        assert "location" in response_headers
+        assert self.scheme is not None
+        assert self.host is not None
+        
+        redirect_url = response_headers["location"]
+        # If the redirect URL is relative, make it absolute
+        if redirect_url.startswith("/"):
+            self.path = redirect_url
+            return self.request()
+        else:
+            self.init_url(redirect_url)
+            return self.request()
+    
+    def cache_response(self, content, response_headers):
+        if self.host is not None and responses_cache.get(self.host + self.path) is None:
+            if "cache-control" in response_headers:
+                cache_control = response_headers["cache-control"]
+                # If the cache control contains max-age or no-cache, 
+                if "max-age" in cache_control:
+                    print("Caching response for {} seconds".format(cache_control.split("=")[1]))
+                    responses_cache[self.host + self.path] = {
+                        "content": content,
+                        "response_headers": response_headers,
+                        "created": time.time(),
+                        "expires": time.time() + int(cache_control.split("=")[1])
+                    }
+
+
+def lex(body):
+    text = ""
+    if not body.startswith("<"):
+        text += body
+        return text
+
+    # TODO: This code is trash, maybe improve it at some point
+    in_tag = False
+    in_entity = False
+    entity = ""
+    for c in body:
+        if c == "<":
+            in_tag = True
+        elif c == ">":
+            in_tag = False
+        elif c == "&":
+            entity = "&"
+            in_entity = True
+        elif c == ";":
+            entity += c
+            decoded_entity = decode_entity(entity)
+            if decoded_entity is not None:
+                text += decoded_entity
+            else:
+                text += entity
+            entity = ""
+            in_entity = False
+        elif in_entity:
+            entity += c
+            if len(entity) > 5:
+                text += entity
+                entity = ""
+                in_entity = False      
+        elif not in_tag and not in_entity:
+            text += c
+    return text
+    
+def decode_entity(entity):
+    if entity == "&lt;":
+        return "<"
+    elif entity == "&gt;":
+        return ">"
+    else:
+        return None
