f49b42b3544ee7983266b350eeed6ecebe4f13ea
joanvilarrasa <joan.vilarrasa96@gmail.com>
2025-07-19 11:42:38 +0200

    Chapter_2-4 Scrollbar

--- browser.py
+++ browser.py
@@ -8,10 +8,6 @@ SCROLL_STEP = 100
 class Browser:
     def __init__(self):
         # Setup window
-        # self.window_properties = {
-        #     "width": WIDTH,
-        #     "height": HEIGHT,
-        # }
         self.window = tkinter.Tk()
         self.canvas = tkinter.Canvas(
             self.window,
@@ -25,7 +21,12 @@ class Browser:
         }
 
         # Setup other state
-        self.scroll = 0
+        self.scroll = {
+            "value": 0,
+            "max": 0,
+            "show": False,
+            "bar_width": 10,
+        }
 
         # Setup bindings
         self.window.bind("<Configure>", self.resize)
@@ -38,31 +39,49 @@ class Browser:
         body = url.request()
         text = lex(body)
         self.content["text"] = text
-        self.content["display_list"] = layout(self.content["text"], self.canvas.winfo_width())
+        self.content["display_list"] = layout(self.content["text"], self.canvas.winfo_width() - self.scroll["bar_width"])
+        self.scroll["max"] = self.content["display_list"][-1][1]
         self.draw()
 
     def draw(self):
         self.canvas.delete("all")
         for x, y, c in self.content["display_list"]:
-            if y > self.scroll + self.canvas.winfo_height(): continue
-            if y + VSTEP < self.scroll: continue
-            self.canvas.create_text(x, y - self.scroll, text=c)
+            if y > self.scroll["value"] + self.canvas.winfo_height(): continue
+            if y + VSTEP < self.scroll["value"]: continue
+            self.canvas.create_text(x, y - self.scroll["value"], text=c)
+
+        if self.scroll["show"]:
+            bar_height = self.canvas.winfo_height() * self.canvas.winfo_height() / self.scroll["max"]
+            bar_y = self.scroll["value"] * (self.canvas.winfo_height() - bar_height) / self.scroll["max"]
+            self.canvas.create_rectangle(
+                self.canvas.winfo_width() - self.scroll["bar_width"],
+                bar_y,
+                self.canvas.winfo_width(),
+                bar_y + bar_height,
+                fill="#f6c198",
+                outline="#f6c198",
+            )
 
 
     # Event handlers
     def scrolldown(self, e):
-        self.scroll += SCROLL_STEP
-        self.draw()
+        if self.scroll["value"] < self.scroll["max"]:
+            self.scroll["value"] += SCROLL_STEP
+            self.draw()
 
     def scrollup(self, e):
-        if self.scroll > 0:
-            self.scroll -= SCROLL_STEP
+        if self.scroll["value"] > 0:
+            self.scroll["value"] -= SCROLL_STEP
             self.draw()
 
     def resize(self, e):
         self.canvas.config(width=e.width, height=e.height)
-        self.content["display_list"] = layout(self.content["text"], e.width)
+        self.content["display_list"] = layout(self.content["text"], e.width - self.scroll["bar_width"])
+        self.scroll["max"] = self.content["display_list"][-1][1] - self.canvas.winfo_height()
+        self.scroll["show"] = self.scroll["max"] > 0
         self.draw()
+
+    
 
 
 # Helpers
@@ -79,7 +98,6 @@ def layout(text, max_width):
             cursor_y += VSTEP
             cursor_x = HSTEP
     return display_list
-
 
 if __name__ == "__main__":
     import sys
