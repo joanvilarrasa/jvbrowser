d2a38d7083debcab29a4fd7473b8acd841171ce7
joanvilarrasa <joan.vilarrasa96@gmail.com>
2025-07-19 13:25:27 +0200

    Chapter_2-7 Alternate text directions

--- browser.py
+++ browser.py
@@ -1,8 +1,6 @@
 from emoji import EmojiProvider, is_emoji
 from url import URL, lex
 import tkinter
-import os
-import unicodedata
 
 WIDTH, HEIGHT = 800, 600
 HSTEP, VSTEP = 13, 18
@@ -30,6 +28,12 @@ class Browser:
             "show": False,
             "bar_width": 10,
         }
+        self.layout_config = {
+            "text_direction": "ltr",    # ltr, rtl
+            "text_align": "left",       # left, right
+        }
+
+        # Setup providers
         self.emoji_provider = EmojiProvider()
 
         # Setup bindings
@@ -43,7 +47,12 @@ class Browser:
         body = url.request()
         text = lex(body)
         self.content["text"] = text
-        self.content["display_list"] = layout(self.content["text"], self.canvas.winfo_width() - self.scroll["bar_width"])
+        self.content["display_list"] = layout(
+            self.content["text"], 
+            self.canvas.winfo_width() - self.scroll["bar_width"],
+            self.layout_config["text_direction"],
+            self.layout_config["text_align"]
+        )
         if len(self.content["display_list"]) > 0:
             self.scroll["max"] = self.content["display_list"][-1][1]
         else:
@@ -95,7 +104,12 @@ class Browser:
 
     def resize(self, e):
         self.canvas.config(width=e.width, height=e.height)
-        self.content["display_list"] = layout(self.content["text"], e.width - self.scroll["bar_width"])
+        self.content["display_list"] = layout(
+            self.content["text"], 
+            e.width - self.scroll["bar_width"],
+            self.layout_config["text_direction"],
+            self.layout_config["text_align"]
+        )
         if len(self.content["display_list"]) > 0:
             self.scroll["max"] = self.content["display_list"][-1][1] - self.canvas.winfo_height()
         else:
@@ -106,21 +120,79 @@ class Browser:
 
 
 # Helpers
-def layout(text, max_width):
+def layout(text, max_width, text_direction="ltr", text_align="left"):
     display_list = []
-    cursor_x, cursor_y = HSTEP, VSTEP
-    for c in text:
-        display_list.append((cursor_x, cursor_y, c))
-        cursor_x += HSTEP
+
+    if text_direction == "rtl":
+        cursor_x_initial_value = max_width - HSTEP
+        cursor_x_increment = -HSTEP
+        cursor_x_max_value = HSTEP
+    else:
+        cursor_x_initial_value = HSTEP
+        cursor_x_increment = HSTEP
+        cursor_x_max_value = max_width - HSTEP
+
+    cursor_x = cursor_x_initial_value
+    cursor_y = VSTEP
+
+    current_line = []
+    for c in text: 
+        current_line.append((cursor_x, cursor_y, c))
+        cursor_x += cursor_x_increment
         if c == "\n":
+            if text_direction == "rtl" and text_align == "left":
+                offset_line_x(current_line, -cursor_x)
+            elif text_direction == "ltr" and text_align == "right":
+                offset_line_x(current_line, max_width - cursor_x)
+
+            display_list.extend(current_line)
+            current_line = []
             cursor_y += VSTEP * 1.5
-            cursor_x = HSTEP
-        elif cursor_x >= max_width - HSTEP:
+            cursor_x = cursor_x_initial_value
+        elif cursor_x_overflow(cursor_x, cursor_x_max_value, text_direction):
+            display_list.extend(current_line)
+            current_line = []
             cursor_y += VSTEP
-            cursor_x = HSTEP
+            cursor_x = cursor_x_initial_value
     return display_list
+
+def cursor_x_overflow(cursor_x, cursor_x_max_value, text_direction):
+    if text_direction == "ltr":
+        return cursor_x >= cursor_x_max_value
+    else:
+        return cursor_x <= cursor_x_max_value
+
+def offset_line_x(line, offset):
+    for i in range(len(line)):
+        cursor_x, cursor_y, c = line[i]
+        line[i] = (cursor_x + offset, cursor_y, c)
+
 
 if __name__ == "__main__":
     import sys
-    Browser().load(URL(sys.argv[1]))
+
+    # Default values
+    text_direction = "ltr"
+    text_align = "left"
+    url = None
+
+    # Parse command line arguments
+    args = sys.argv[1:]
+    url = None
+    for arg in args:
+        if arg.startswith("--text_direction="):
+            text_direction = arg.split("=", 1)[1]
+        elif arg.startswith("--text_align="):
+            text_align = arg.split("=", 1)[1]
+        elif not arg.startswith("--"):
+            url = arg
+
+    if url is None:
+        print("Usage: python browser.py [--text_direction=ltr|rtl] [--text_align=left|right] <url>")
+        sys.exit(1)
+
+    browser = Browser()
+    browser.layout_config["text_direction"] = text_direction
+    browser.layout_config["text_align"] = text_align
+    browser.load(URL(url))
     tkinter.mainloop()
