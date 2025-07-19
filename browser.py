874ad619a72788fff6e18aff61e93998a0839ad0
joanvilarrasa <joan.vilarrasa96@gmail.com>
2025-07-19 11:05:22 +0200

    Chapter_2-3 Resizing

--- browser.py
+++ browser.py
@@ -7,15 +7,28 @@ SCROLL_STEP = 100
 
 class Browser:
     def __init__(self):
+        # Setup window
+        # self.window_properties = {
+        #     "width": WIDTH,
+        #     "height": HEIGHT,
+        # }
         self.window = tkinter.Tk()
         self.canvas = tkinter.Canvas(
-            self.window, 
-            width=WIDTH,
-            height=HEIGHT
+            self.window,
         )
-        self.canvas.pack()
+        self.canvas.pack(fill=tkinter.BOTH, expand=True)
+
+        # Setup content
+        self.content = {
+            "text": None,
+            "display_list": [],
+        }
+
+        # Setup other state
         self.scroll = 0
-        self.display_list = []
+
+        # Setup bindings
+        self.window.bind("<Configure>", self.resize)
         self.window.bind("<Down>", self.scrolldown)
         self.window.bind("<Up>", self.scrollup)
         self.window.bind("<Button-4>", self.scrollup)
@@ -24,16 +37,19 @@ class Browser:
     def load(self, url):
         body = url.request()
         text = lex(body)
-        self.display_list = layout(text)
+        self.content["text"] = text
+        self.content["display_list"] = layout(self.content["text"], self.canvas.winfo_width())
         self.draw()
 
     def draw(self):
         self.canvas.delete("all")
-        for x, y, c in self.display_list:
-            if y > self.scroll + HEIGHT: continue
+        for x, y, c in self.content["display_list"]:
+            if y > self.scroll + self.canvas.winfo_height(): continue
             if y + VSTEP < self.scroll: continue
             self.canvas.create_text(x, y - self.scroll, text=c)
 
+
+    # Event handlers
     def scrolldown(self, e):
         self.scroll += SCROLL_STEP
         self.draw()
@@ -43,7 +59,14 @@ class Browser:
             self.scroll -= SCROLL_STEP
             self.draw()
 
-def layout(text):
+    def resize(self, e):
+        self.canvas.config(width=e.width, height=e.height)
+        self.content["display_list"] = layout(self.content["text"], e.width)
+        self.draw()
+
+
+# Helpers
+def layout(text, max_width):
     display_list = []
     cursor_x, cursor_y = HSTEP, VSTEP
     for c in text:
@@ -52,7 +75,7 @@ def layout(text):
         if c == "\n":
             cursor_y += VSTEP * 1.5
             cursor_x = HSTEP
-        elif cursor_x >= WIDTH - HSTEP:
+        elif cursor_x >= max_width - HSTEP:
             cursor_y += VSTEP
             cursor_x = HSTEP
     return display_list
